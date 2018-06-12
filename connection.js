f2eef8115432a0d014ed36033ba7873e191fada4
Uilton Santos <uilton.jose@gmail.com>
2018-06-12 16:29:46 +0200

    make connection DB

--- connection.js
+++ connection.js
@@ -2,10 +2,10 @@ var util      = require('util');
 var mysql      = require('mysql');
 
 var connection = mysql.createConnection({  
-    host     : 'helpnet.ccyhvv2orx1w.us-east-1.rds.amazonaws.com',
-    user     : 'admin',
-    password : 'h3lpn3ts',
-    database : 'helpnet'
+    host     : 'lt80glfe2gj8p5n2.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306',
+    user     : 'wnxoormb91xkfef9',
+    password : 'qmwan6b8lamtbp9j',
+    database : 's0xdx9gvx8au1ooc'
 });
 
 module.exports = {
