67b370b67b85fa5aae067234768bcab2d6b585c6
Santos, Uilton (Developer) <uilton.santos@sky.uk>
2018-06-26 15:07:50 +0100

    novo deploy --- hrs > 15.07 |  dia > 26.06

--- connection.js
+++ connection.js
@@ -10,21 +10,21 @@ var mysql  = require('mysql');
 // });
 
 //HEROKU
-/*
+
 var connection = mysql.createConnection({  
     host     : 'lt80glfe2gj8p5n2.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
     user     : 'wnxoormb91xkfef9',
     password : 'qmwan6b8lamtbp9j',
     database : 's0xdx9gvx8au1ooc'
 });
-*/
+
 //DEV
- var connection = mysql.createConnection({  
-     host     : 'localhost',
-     user     : 'root',
-     password : 'root',
-     database : 'helpnet'
- });
+//  var connection = mysql.createConnection({  
+//      host     : 'localhost',
+//      user     : 'root',
+//      password : 'root',
+//      database : 'helpnet'
+//  });
 
 module.exports = {
        
